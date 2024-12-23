7e08f804145bb56c0b6804fe98a17e0a110271ca
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-12-23 10:24:15 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/derivedpuma7/ci
+
+go 1.21.5
---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	a := 10
+	b := 20
+
+	total := Sum(a, b)
+
+	if(total != 30) {
+		t.Errorf("Invalid result. Expected: %d, Got: %d", a+b, total)
+	}
+}
